7566638adc0a77cb1f046e60a8f6ff5806494237
Kao <a21943316@gmail.com>
2024-03-30 17:15:36 +0800

    new search

--- reviews-analytics13.py
+++ reviews-analytics13.py
@@ -5,7 +5,7 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 10000 == 0:
 			print( len(data) )
 
 print( 'END read file')
@@ -15,3 +15,10 @@ for d in data:
 	sum_len += len(d)
 
 print('平均留言長度為', sum_len/len(data) )
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+
+print('一共有', len(new), '筆留言長度小於100')
